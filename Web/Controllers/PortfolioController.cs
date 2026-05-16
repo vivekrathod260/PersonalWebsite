86fa1c587aaf5f220077e09ae42791016d8a04bb
VivekRathod <VivekRathod@VIVEK-LAPTOP>
2026-05-17 01:07:21 +0530

    Implemented caching for get apis

--- Web/Controllers/PortfolioController.cs
+++ Web/Controllers/PortfolioController.cs
@@ -16,6 +16,7 @@ public class PortfolioController : ControllerBase
         _portfolioService = portfolioService;
     }
 
+    [ResponseCache(Duration = 600, Location = ResponseCacheLocation.Any)]
     [HttpGet("profile")]
     public async Task<ActionResult<ProfileDto>> GetProfile()
     {
@@ -24,42 +25,49 @@ public class PortfolioController : ControllerBase
         return Ok(profile);
     }
 
+    [ResponseCache(Duration = 300, Location = ResponseCacheLocation.Any)]
     [HttpGet("projects")]
     public async Task<ActionResult<List<ProjectDto>>> GetProjects()
     {
         return Ok(await _portfolioService.GetProjectsAsync());
     }
 
+    [ResponseCache(Duration = 300, Location = ResponseCacheLocation.Any)]
     [HttpGet("projects/featured")]
     public async Task<ActionResult<List<ProjectDto>>> GetFeaturedProjects()
     {
         return Ok(await _portfolioService.GetFeaturedProjectsAsync());
     }
 
+    [ResponseCache(Duration = 300, Location = ResponseCacheLocation.Any)]
     [HttpGet("experiences")]
     public async Task<ActionResult<List<ExperienceDto>>> GetExperiences()
     {
         return Ok(await _portfolioService.GetExperiencesAsync());
     }
 
+    [ResponseCache(Duration = 600, Location = ResponseCacheLocation.Any)]
     [HttpGet("skills")]
     public async Task<ActionResult<List<SkillCategoryDto>>> GetSkills()
     {
         return Ok(await _portfolioService.GetSkillsGroupedAsync());
     }
 
+    [ResponseCache(Duration = 600, Location = ResponseCacheLocation.Any)]
     [HttpGet("testimonials")]
     public async Task<ActionResult<List<TestimonialDto>>> GetTestimonials()
     {
         return Ok(await _portfolioService.GetTestimonialsAsync());
     }
 
+    [ResponseCache(Duration = 600, Location = ResponseCacheLocation.Any)]
     [HttpGet("socials")]
     public async Task<ActionResult<List<SocialDto>>> GetSocials()
     {
         return Ok(await _portfolioService.GetSocialsAsync());
     }
 
+    [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any)]
     [HttpGet("settings")]
     public async Task<ActionResult<SettingsDto?>> GetSettings()
     {
